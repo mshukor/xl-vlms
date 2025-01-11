8694ff700c213eb094b03d6788edc88b72641763
Jayneel PAREKH <parekh@gpu.scai.sorbonne-universite.fr>
2025-01-11 11:48:01 +0100

    Local interpretation added in visualization notebook

--- src/examples/concept_dictionary/feature_decomposition.sh
+++ src/examples/concept_dictionary/feature_decomposition.sh
@@ -4,15 +4,15 @@
 cd ~/xl-vlms
 
 #model_name=llava-hf/llava-1.5-7b-hf
-#model_name=allenai/Molmo-7B-D-0924
+model_name=allenai/Molmo-7B-D-0924
 #model_name=HuggingFaceM4/idefics2-8b
-model_name=Qwen/Qwen2-VL-7B-Instruct
+#model_name=Qwen/Qwen2-VL-7B-Instruct
 
 # Need to specify analysis type. This will decompose features, and extract both text, visual grounding
 analysis_name=decompose_activations_text_grounding_image_grounding
 
 # Specify path where you have saved features on training data
-saved_features_path=/home/parekh/features/save_hidden_states_for_token_of_interest_qwen2_train_generation_split_train.pth
+saved_features_path=/home/parekh/features/save_hidden_states_for_token_of_interest_molmo_train_generation_split_train.pth
 
 # Where to store details about extracted concepts. Default directory is results/
 results_filename=results_train
@@ -25,13 +25,13 @@ results_filename=results_train
 
 # Molmo-7B
 #feature_module=model.transformer.blocks.27
-#feature_module=model.transformer.ln_f
+feature_module=model.transformer.ln_f
 
 # Idefics2-8B
 #feature_module=model.text_model.norm
 
 # Qwen2-VL-7B
-feature_module=model.norm
+#feature_module=model.norm
 
 decomposition=snmf # Current options: snmf, kmeans, pca, simple
 n_concepts=20 # Size of dictionary learnt i.e. number of concepts
